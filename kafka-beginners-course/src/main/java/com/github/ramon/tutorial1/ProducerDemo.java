927288c12793b54a795cc7fc01d8438e2bc63637
ramon.queiroga <rqueiroga@rq>
2020-04-11 19:24:17 -0300

    Creating a simple producer and a producer with callback

---
+++ kafka-beginners-course/src/main/java/com/github/ramon/tutorial1/ProducerDemo.java
@@ -0,0 +1,34 @@
+package com.github.ramon.tutorial1;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
+public class ProducerDemo {
+
+    public static void main(String[] args) {
+        final String bootstrapServer = "127.0.0.1:9092";
+
+        //create producer properties
+        Properties properties = new Properties();
+        //Server address. Connecting in one broker, we connect to all brokers
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServer);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        //create the producer
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
+
+        //create a producer record
+        ProducerRecord<String, String> record = new ProducerRecord<String, String>("ramon-topic", "my first producer in java!");
+
+        //send data
+        producer.send(record);
+
+        producer.close();
+    }
+
+}
---
+++ kafka-beginners-course/src/main/java/com/github/ramon/tutorial1/ProducerDemoWithCallback.java
@@ -0,0 +1,50 @@
+package com.github.ramon.tutorial1;
+
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+
+public class ProducerDemoWithCallback {
+
+    private static final String TOPIC = "first_topic";
+    private static final Logger LOG = LoggerFactory.getLogger(ProducerDemoWithCallback.class);
+    private static final String BOOTSTRAP_SERVER = "127.0.0.1:9092";
+
+    public static void main(String[] args) {
+        //create producer properties
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVER);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        //create the producer
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
+
+        for(int i = 0; i < 10; i++) {
+            //create a producer record
+            ProducerRecord<String, String> record = new ProducerRecord<String, String>(TOPIC, "Producer with callback: " + i);
+            producer.send(record, new Callback() {
+                public void onCompletion(RecordMetadata metadata, Exception e) {
+                    if(e == null) {
+                        LOG.info("Received metadata.  + \n" +
+                                                 "Topic: " + metadata.topic() + "\n" +
+                                                 "Partition: " + metadata.partition() + "\n" +
+                                                 "Offset: " + metadata.offset() + "\n" +
+                                                 "Timestamp: " + metadata.timestamp());
+                    } else {
+                        LOG.error("An error has ocurred: " + e.getMessage());
+                    }
+                }
+            });
+
+            producer.flush();
+        }
+
+        producer.close();
+    }
+
+}
