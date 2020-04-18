58728f0a7828866eca56b06862cb635eb690c1bb
ramon.queiroga <rqueiroga@rq>
2020-04-18 10:51:30 -0300

    producer with key

---
+++ kafka-beginners-course/src/main/java/com/github/ramon/tutorial1/ProducerDemoWithKey.java
@@ -0,0 +1,53 @@
+package com.github.ramon.tutorial1;
+
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+public class ProducerDemoWithKey {
+
+    private static final String TOPIC = "first_topic";
+    private static final Logger LOG = LoggerFactory.getLogger(ProducerDemoWithKey.class);
+    private static final String BOOTSTRAP_SERVER = "127.0.0.1:9092";
+
+    public static void main(String[] args) {
+        //create producer properties
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVER);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        //create the producer
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
+
+        for(int i = 0; i < 10; i++) {
+            //create a producer record
+            String message = "Producer with key " + i;
+            String key = "id_" + i;
+            LOG.info("Key: " + key);
+
+            ProducerRecord<String, String> record = new ProducerRecord<String, String>(TOPIC, key, message);
+            producer.send(record, new Callback() {
+                public void onCompletion(RecordMetadata metadata, Exception e) {
+                    if(e == null) {
+                        LOG.info("Received metadata.  + \n" +
+                                                 "Topic: " + metadata.topic() + "\n" +
+                                                 "Partition: " + metadata.partition() + "\n" +
+                                                 "Offset: " + metadata.offset() + "\n" +
+                                                 "Timestamp: " + metadata.timestamp());
+                    } else {
+                        LOG.error("An error has ocurred: " + e.getMessage());
+                    }
+                }
+            });
+
+            producer.flush();
+        }
+
+        producer.close();
+    }
+
+}
