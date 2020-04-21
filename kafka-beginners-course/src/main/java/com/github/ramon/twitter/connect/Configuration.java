84afa94f537dda9f478e784303d927eeeeada88b
ramon.queiroga <rqueiroga@rq>
2020-04-21 18:50:37 -0300

    Including compression to the messages of Kafka. This is a important configuration when you have a high throughput and large messages.

--- kafka-beginners-course/src/main/java/com/github/ramon/twitter/connect/Configuration.java
+++ kafka-beginners-course/src/main/java/com/github/ramon/twitter/connect/Configuration.java
@@ -12,6 +12,9 @@ public class Configuration {
     private static final String BOOTSTRAP_SERVER = "127.0.0.1:9092";
     private static final String REGEX    = "=";
     private static final String FILENAME = "keys.txt";
+    private static final String ALL = "all";
+    private static final String MAX_IN_FLIGHT_REQUESTS = "5";
+    private static final String COMPRESSION = "lz4";
 
     static Properties retrieveAccessConfig() {
         InputStream inputStream = readFile();
@@ -34,9 +37,12 @@ public class Configuration {
 
         //Configuring a safe producer. This config ensures that the producer will not duplicate information on resend the message to kafka
         properties.setProperty(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, "true");
-        properties.setProperty(ProducerConfig.ACKS_CONFIG, "all");
+        properties.setProperty(ProducerConfig.ACKS_CONFIG, ALL);
         properties.setProperty(ProducerConfig.RETRIES_CONFIG, Integer.toString(Integer.MAX_VALUE));
-        properties.setProperty(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, "5");
+        properties.setProperty(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, MAX_IN_FLIGHT_REQUESTS);
+
+        //configuring compression to the producer. This config is only producer level, no broker or consumer need to configure compression.
+        properties.setProperty(ProducerConfig.COMPRESSION_TYPE_CONFIG, COMPRESSION);
         return properties;
     }
 
